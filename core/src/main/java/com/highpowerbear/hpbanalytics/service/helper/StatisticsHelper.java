bb25a59b7bbed6d00185907122d99b9a48e24ab4
Robert Kolar <robert.kolar@highpowerbear.com>
2022-01-24 11:09:04 +0100

    Upgrade hazelcast version to 5.0.2, update log props, fix imports

--- core/src/main/java/com/highpowerbear/hpbanalytics/service/helper/StatisticsHelper.java
+++ core/src/main/java/com/highpowerbear/hpbanalytics/service/helper/StatisticsHelper.java
@@ -18,7 +18,6 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.highpowerbear.hpbanalytics.config.HanSettings.ALL;
