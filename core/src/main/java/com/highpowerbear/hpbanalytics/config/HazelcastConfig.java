9d2f4d1b5c474e10fd0c2ecf7dff35637ee52825
Robert Kolar <robert.kolar@highpowerbear.com>
2022-01-24 15:22:28 +0100

    Modify hazelcast instance bean name

--- core/src/main/java/com/highpowerbear/hpbanalytics/config/HazelcastConfig.java
+++ core/src/main/java/com/highpowerbear/hpbanalytics/config/HazelcastConfig.java
@@ -23,43 +23,39 @@ import java.util.concurrent.BlockingQueue;
 public class HazelcastConfig {
     private static final Logger log = LoggerFactory.getLogger(HazelcastConfig.class);
 
-    @Bean("hanHazelcastInstance")
-    public HazelcastInstance hanHazelcastInstance() {
-
+    @Bean
+    public HazelcastInstance hazelcastInstance() {
         Config config = new Config(HanSettings.HAZELCAST_INSTANCE_NAME)
                 .addQueueConfig(executionQueueConfig())
                 .addMapConfig(exchangeRateMapConfig())
                 .addMapConfig(statisticsMapConfig())
                 .addMapConfig(currentStatisticsMapConfig());
 
-        log.info("hazelcast queue configs " + config.getQueueConfigs());
-        log.info("hazelcast map configs " + config.getMapConfigs());
-
         return Hazelcast.newHazelcastInstance(config);
     }
 
     @Bean
-    @DependsOn("hanHazelcastInstance")
+    @DependsOn("hazelcastInstance")
     public Map<String, ExchangeRateDTO> exchangeRateMap() {
-        return hanHazelcastInstance().getMap(HanSettings.HAZELCAST_EXCHANGE_RATE_MAP_NAME);
+        return hazelcastInstance().getMap(HanSettings.HAZELCAST_EXCHANGE_RATE_MAP_NAME);
     }
 
     @Bean
-    @DependsOn("hanHazelcastInstance")
+    @DependsOn("hazelcastInstance")
     public BlockingQueue<ExecutionDTO> executionQueue() {
-        return hanHazelcastInstance().getQueue(HanSettings.HAZELCAST_EXECUTION_QUEUE_NAME);
+        return hazelcastInstance().getQueue(HanSettings.HAZELCAST_EXECUTION_QUEUE_NAME);
     }
 
     @Bean
-    @DependsOn("hanHazelcastInstance")
+    @DependsOn("hazelcastInstance")
     public Map<String, List<Statistics>> statisticsMap() {
-        return hanHazelcastInstance().getMap(HanSettings.HAZELCAST_STATISTICS_MAP_NAME);
+        return hazelcastInstance().getMap(HanSettings.HAZELCAST_STATISTICS_MAP_NAME);
     }
 
     @Bean
-    @DependsOn("hanHazelcastInstance")
+    @DependsOn("hazelcastInstance")
     public Map<String, List<Statistics>> currentStatisticsMap() {
-        return hanHazelcastInstance().getMap(HanSettings.HAZELCAST_CURRENT_STATISTICS_MAP_NAME);
+        return hazelcastInstance().getMap(HanSettings.HAZELCAST_CURRENT_STATISTICS_MAP_NAME);
     }
 
     private QueueConfig executionQueueConfig() {
